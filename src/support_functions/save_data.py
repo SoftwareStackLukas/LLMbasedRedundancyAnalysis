5faa7aa1e356ce982041e06b6d5bbf485f4a647f
SoftwareStackLukas <Lukas-Sebastian.Hofmann@gmx.de>
2024-05-17 11:43:50 +0200

    using standard libaries

--- src/support_functions/save_data.py
+++ src/support_functions/save_data.py
@@ -3,7 +3,7 @@ import json
 
 def save_to_json_persistent(folder_name: str, json_collection: any):
     base_path: str = os.getcwd()
-    folder_path: str = base_path + '/results/'
+    folder_path: str = os.path.join(base_path, "results")
     os.makedirs(folder_path, exist_ok=True)
     folder_path += folder_name
     os.makedirs(folder_path, exist_ok=True)
@@ -12,8 +12,10 @@ def save_to_json_persistent(folder_name: str, json_collection: any):
     idx: int = 0
     for key in json_collection:
         idx = 0
-        while os.path.exists(f"{folder_path}/{idx:02d}_{key}.json"):
+        temp: str = os.path.join(folder_path, f"{idx:02d}_{key}.json")
+        while os.path.exists(temp):
             idx += 1
-        json_file_path = f"{folder_path}/{idx:02d}_{key}.json"
+            temp = os.path.join(folder_path, f"{idx:02d}_{key}.json")
+        json_file_path = temp
         with open(json_file_path, 'w') as json_file:
             json.dump(json_collection[key], json_file, indent=4)
