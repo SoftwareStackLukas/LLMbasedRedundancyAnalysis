8d531a5380f307ee369f47e76b1934e4e22ab613
SoftwareStackLukas <Lukas-Sebastian.Hofmann@gmx.de>
2024-05-17 11:57:31 +0200

    Simplifing the code and adding doc

--- src/support_functions/save_data.py
+++ src/support_functions/save_data.py
@@ -3,19 +3,17 @@ import json
 
 def save_to_json_persistent(folder_name: str, json_collection: any):
     base_path: str = os.getcwd()
-    folder_path: str = os.path.join(base_path, "results")
-    os.makedirs(folder_path, exist_ok=True)
-    folder_path += folder_name
+    folder_path: str = os.path.join(base_path, "results", folder_name)
     os.makedirs(folder_path, exist_ok=True)
 
-    json_file_path:str = ""
+    unique_json_file_path:str = ""
     idx: int = 0
     for key in json_collection:
         idx = 0
-        temp: str = os.path.join(folder_path, f"{idx:02d}_{key}.json")
-        while os.path.exists(temp):
+        unique_json_file_path: str = os.path.join(folder_path, f"{idx:02d}_{key}.json")
+        while os.path.exists(unique_json_file_path):
             idx += 1
-            temp = os.path.join(folder_path, f"{idx:02d}_{key}.json")
-        json_file_path = temp
-        with open(json_file_path, 'w') as json_file:
+            unique_json_file_path = os.path.join(folder_path, f"{idx:02d}_{key}.json")
+        with open(unique_json_file_path, 'w') as json_file:
             json.dump(json_collection[key], json_file, indent=4)
+        unique_json_file_path = None
